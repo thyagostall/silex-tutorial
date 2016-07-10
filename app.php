5e70acdc3ba11c677688ad531095bee8915e88c7
thyagostall <thstall@gmail.com>
2016-07-10 11:36:43 -0300

    Added JWT security provider

--- app.php
+++ app.php
@@ -3,6 +3,10 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\InMemoryUserProvider;
 use Tutorial\Controller\UserController;
 
 $config = require_once __DIR__ . "/config/config.php";
@@ -12,6 +16,49 @@ if (!$config || !is_array($config)) {
 
 $app = new Application();
 $app["debug"] = true;
+
+$app['security.jwt'] = [
+    'secret_key' => 'Very_secret_key',
+    'life_time'  => 86400,
+    'options'    => [
+        'username_claim' => 'sub', // default name, option specifying claim containing username
+        'header_name' => 'X-Access-Token', // default null, option for usage normal oauth2 header
+        'token_prefix' => 'Bearer',
+    ]
+];
+
+$app['users'] = function () use ($app) {
+    $users = [
+        'admin' => array(
+            'roles' => array('ROLE_ADMIN'),
+            // raw password is foo
+            'password' => '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg==',
+            'enabled' => true
+        ),
+    ];
+
+    return new InMemoryUserProvider($users);
+};
+
+$app->register(new Silex\Provider\SecurityServiceProvider());
+$app->register(new Silex\Provider\SecurityJWTServiceProvider());
+
+$app['security.firewalls'] = array(
+    'login' => [
+        'pattern' => 'login|register|oauth',
+        'anonymous' => true,
+    ],
+    'secured' => array(
+        'pattern' => '^.*$',
+        'logout' => array('logout_path' => '/logout'),
+        'users' => $app['users'],
+        'jwt' => array(
+            'use_forward' => true,
+            'require_previous_session' => false,
+            'stateless' => true,
+        )
+    ),
+);
 
 $app->register(new \Silex\Provider\DoctrineServiceProvider(), [
     'db.options' => $config['db.options']
@@ -38,4 +85,36 @@ $app->get('/', function() use ($app) {
     $controller = new UserController();
     return $controller->allUsers($app);
 });
+
+$app->post('/login', function(Request $request) use ($app){
+    $vars = json_decode($request->getContent(), true);
+
+    try {
+        if (empty($vars['_username']) || empty($vars['_password'])) {
+            throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $vars['_username']));
+        }
+
+        /**
+         * @var $user User
+         */
+        $user = $app['users']->loadUserByUsername($vars['_username']);
+
+        if (! $app['security.encoder.digest']->isPasswordValid($user->getPassword(), $vars['_password'], '')) {
+            throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $vars['_username']));
+        } else {
+            $response = [
+                'success' => true,
+                'token' => $app['security.jwt.encoder']->encode(['name' => $user->getUsername()]),
+            ];
+        }
+    } catch (UsernameNotFoundException $e) {
+        $response = [
+            'success' => false,
+            'error' => 'Invalid credentials',
+        ];
+    }
+
+    return $app->json($response, ($response['success'] == true ? Response::HTTP_OK : Response::HTTP_BAD_REQUEST));
+});
+
 $app->run();
